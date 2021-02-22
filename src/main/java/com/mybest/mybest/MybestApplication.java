b3920157b9451b0d03876d53389731efeffd1a47
sagar adhikari <sagaradhikari@sagars-MacBook-Pro.local>
2021-02-21 22:38:08 -0600

    mero pahelo project

---
+++ src/main/java/com/mybest/mybest/MybestApplication.java
@@ -0,0 +1,15 @@
+package com.mybest.mybest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybestApplication.class, args);
+	}
+
+
+
+}
---
+++ src/test/java/com/mybest/mybest/MybestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybest.mybest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
